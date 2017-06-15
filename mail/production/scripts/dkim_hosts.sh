d349950be1248b06aa80c700e5dfe2f6d7b07b5d
Denis Zorinets <deniszorinets@gmail.com>
2017-06-15 20:48:21 +0300

    add multiple mail domain support

--- mail/production/scripts/dkim_hosts.sh
+++ mail/production/scripts/dkim_hosts.sh
@@ -1,16 +1,24 @@
 #/bin/sh
 
-echo default._domainkey.${MAIL_DOMAIN} ${MAIL_DOMAIN}:default:/var/db/dkim/${MAIL_DOMAIN}/default.private > /etc/opendkim/KeyTable
-echo *@${MAIL_DOMAIN} default._domainkey.${MAIL_DOMAIN} > /etc/opendkim/SigningTable
 echo 127.0.0.1 >> /etc/opendkim/TrustedHosts
-echo ${MAIL_DOMAIN} >> /etc/opendkim/TrustedHosts
-echo ${SENDER_HOST} >> /etc/opendkim/TrustedHosts
 
-if [ ! -f /var/db/dkim/${MAIL_DOMAIN}/default.private ]; then
-    if [ ! -d /var/db/dkim/${MAIL_DOMAIN} ]; then
-        mkdir /var/db/dkim/${MAIL_DOMAIN}
+for h in ${MAIL_DOMAIN}; do
+
+    if [ ! -f /var/db/dkim/${MAIL_DOMAIN}/default.private ]; then
+        if [ ! -d /var/db/dkim/${MAIL_DOMAIN} ]; then
+            mkdir /var/db/dkim/${MAIL_DOMAIN}
+        fi
+        opendkim-genkey -D /var/db/dkim/${MAIL_DOMAIN}/ -d ${MAIL_DOMAIN} -s default
     fi
-    opendkim-genkey -D /var/db/dkim/${MAIL_DOMAIN}/ -d ${MAIL_DOMAIN} -s default
-fi
+
+    echo ${MAIL_DOMAIN} >> /etc/opendkim/TrustedHosts
+    echo default._domainkey.${MAIL_DOMAIN} ${MAIL_DOMAIN}:default:/var/db/dkim/${MAIL_DOMAIN}/default.private > /etc/opendkim/KeyTable
+    echo *@${MAIL_DOMAIN} default._domainkey.${MAIL_DOMAIN} > /etc/opendkim/SigningTable
+done
+
+for h in ${SENDER_HOST}; do
+    echo ${SENDER_HOST} >> /etc/opendkim/TrustedHosts
+done
+
 
 chown -R opendkim:opendkim /var/db/dkim
