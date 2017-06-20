efc06a4412c165247d71c15a9423a508d686d2a1
Denis Zorinets <deniszorinets@gmail.com>
2017-06-20 15:24:14 +0300

    fix for opendkim

--- mail/production/scripts/dkim_hosts.sh
+++ mail/production/scripts/dkim_hosts.sh
@@ -4,20 +4,20 @@ echo 127.0.0.1 >> /etc/opendkim/TrustedHosts
 
 for h in ${MAIL_DOMAIN}; do
 
-    if [ ! -f /var/db/dkim/${MAIL_DOMAIN}/default.private ]; then
-        if [ ! -d /var/db/dkim/${MAIL_DOMAIN} ]; then
-            mkdir /var/db/dkim/${MAIL_DOMAIN}
+    if [ ! -f /var/db/dkim/${h}/default.private ]; then
+        if [ ! -d /var/db/dkim/${h} ]; then
+            mkdir /var/db/dkim/${h}
         fi
-        opendkim-genkey -D /var/db/dkim/${MAIL_DOMAIN}/ -d ${MAIL_DOMAIN} -s default
+        opendkim-genkey -D /var/db/dkim/${h}/ -d ${h} -s default
     fi
 
-    echo ${MAIL_DOMAIN} >> /etc/opendkim/TrustedHosts
-    echo default._domainkey.${MAIL_DOMAIN} ${MAIL_DOMAIN}:default:/var/db/dkim/${MAIL_DOMAIN}/default.private > /etc/opendkim/KeyTable
-    echo *@${MAIL_DOMAIN} default._domainkey.${MAIL_DOMAIN} > /etc/opendkim/SigningTable
+    echo ${h} >> /etc/opendkim/TrustedHosts
+    echo default._domainkey.${h} ${h}:default:/var/db/dkim/${h}/default.private > /etc/opendkim/KeyTable
+    echo *@${h} default._domainkey.${h} > /etc/opendkim/SigningTable
 done
 
 for h in ${SENDER_HOST}; do
-    echo ${SENDER_HOST} >> /etc/opendkim/TrustedHosts
+    echo ${h} >> /etc/opendkim/TrustedHosts
 done
 
 
